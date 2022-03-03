b03f8a3f7a7952ef2e239a4ea06bdb482734fe7c
Ruvenss G. Wilches <ruvenss@gmail.com>
2022-03-03 21:36:25 +0100

    Update admin.php

--- templates/admin.php
+++ templates/admin.php
@@ -13,7 +13,7 @@ style('rocketchat_nextcloud', 'style');
 ?>
 <div class="rocket-info-wrapper">
         <div class="section">
-            <h2><img src="/apps/rocketchat_nextcloud/img/rocket-logo-black.png" width=15> Rocket Chat v.0.9.5 RC</h2>
+            <h2><img src="/apps/rocketchat_nextcloud/img/rocket-logo-black.png" width=15> Rocket Chat v.0.9.6 RC</h2>
             <div class="row">
                     <div class="col col-6">
                         <div id="rocketURL" class="infobox">
